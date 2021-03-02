f78395951c003ecda51b5dea747a15eb03b6e6a2
phivh <>
2021-03-02 22:48:31 +0700

    request

--- scripts/deploy_dev.js
+++ scripts/deploy_dev.js
@@ -89,12 +89,12 @@ const main = async () => {
         location / {
                 proxy_pass http://127.0.0.1:${PREFIX}${CIRCLE_PULL_REQUEST};
                 proxy_http_version 1.1;
-                proxy_set_header Upgrade ${'$http_upgrade'};
+                proxy_set_header Upgrade ${'$'}${'http_upgrade'};
                 proxy_set_header Connection 'upgrade';
-                proxy_set_header X-Real-IP  ${'$remote_addr'};
-                proxy_set_header X-Forwarded-For ${'$remote_addr'};
-                proxy_set_header Host ${'$host'};
-                proxy_cache_bypass ${'$http_upgrade'};
+                proxy_set_header X-Real-IP ${'$'}${'remote_addr'};
+                proxy_set_header X-Forwarded-For ${'$'}${'remote_addr'};
+                proxy_set_header Host ${'$'}${'host'};
+                proxy_cache_bypass ${'$'}${'http_upgrade'};
         }
       }`;
       
